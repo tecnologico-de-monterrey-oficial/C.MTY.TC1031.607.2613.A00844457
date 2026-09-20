e2aaed53ad47911ee6196c8004e909fe96525bc8
Char <a00844457@tec.mx>
2026-09-19 21:40:36 -0600

    lectura de archivos

--- Evidences/Evidence1/Evidencia1.cpp
+++ Evidences/Evidence1/Evidencia1.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <sstream>
 using namespace std; 
 
+struct Registro {
+    string mes;
+    int dia;
+    int anno;
+    string hora;
+    string ip;
+    string contenido;
+};
 
 
 int main (){
@@ -14,8 +23,8 @@ int main (){
 
     cout<< "=========== Menú================"<< endl; 
     cout<< "Escriba 1 para escoger el archivo  log607-1.txt (desordenado)" <<endl;
-    cout<< "Escriba 2 para  escoger el archivo log607-2.txt (casi ordenado) "<< endl; 
-    cin>>opcionArchivo>>endl;
+    cout<< "Escriba 2 para  escoger el archivo log607-2.txt (casi ordenado)"<< endl; 
+    cin>>opcionArchivo;
 
     if (opcionArchivo == 1){
         archivo.open("log607-1.txt");
@@ -24,9 +33,33 @@ int main (){
         archivo.open("log607-2.txt");
 
     } else {
-        cout<<"Opción iválida"<<endl;
+        cout<<"Error al cargar"<<endl;
     }
+
+    // leer lineas del archivo
+    vector<Registro> registros;
+    string linea;
+
+    while(getline(archivo, linea)){
+        Registro dato;
+        stringstream ss(linea);
+
+        ss>>dato.mes;
+        ss>>dato.dia;
+        ss>>dato.anno;
+
+        getline(ss, dato.contenido);
+        registros.push_back(dato);
+    }
+
+    cout<<"# total de registros: "<<registros.size()<<endl;
+
+    // for (int i = 0; i<registros.size(); i++){
+
+
+    // }
     
+
 
 
 
